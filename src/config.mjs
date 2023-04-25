a60d256acaaaf6099ac02c0837dabf51a6c6db6f
matiasfacello <matiasfacello@gmail.com>
2023-04-24 21:36:01 -0300

    feat: base config file and import path

---
+++ src/config.mjs
@@ -0,0 +1,14 @@
+export const SITE = {
+  name: "Jota Eventos",
+  title: "Jota Eventos",
+  slogan: "Sonido e Iluminación",
+  description: "Todo lo que necesitas para hacer de tu evento algo único.",
+
+  origin: "https://www.jotaeventos.com.ar",
+  basePathname: "/",
+
+  facebook: "",
+  instagram: "https://www.instagram.com/jota_eventos/",
+
+  googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+};
