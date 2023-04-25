61bd9e391f82f2bc30e305ad974955c5542b0bf9
matiasfacello <matiasfacello@gmail.com>
2023-04-24 21:37:28 -0300

    feat: Logo comp with fonts and navbar comp

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
+        hamburger: ["'Hamburger'", ...defaultTheme.fontFamily.sans],
+        walkway: ["'Walkway'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
