29d0598925266a2269bca7dd63d43820b0c3f540
matiasfacello <matiasfacello@gmail.com>
2023-04-24 22:56:45 -0300

    feat: adds astro-compress and some tweaking in cfg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,30 @@
 import { defineConfig } from "astro/config";
+import { SITE } from "./src/config.mjs";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown(), sitemap(), tailwind(), image()],
+  site: SITE.origin,
+  base: SITE.basePathname,
+  output: "static",
+  integrations: [
+    partytown(),
+    sitemap(),
+    tailwind(),
+    image(),
+    compress({
+      css: true,
+      html: {
+        removeAttributeQuotes: false,
+      },
+      img: false,
+      js: true,
+      svg: false,
+      logger: 1,
+    }),
+  ],
 });
