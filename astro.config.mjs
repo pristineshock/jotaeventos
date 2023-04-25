f5131e17b0b1703c337982be64876958b61bb801
matiasfacello <matiasfacello@gmail.com>
2023-04-24 21:02:06 -0300

    feat: adds multiple dependecies
    astro/image, astro/partytown, astro/sitemap,
    astro/tailwind, tailwind, prettier and prettier plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [partytown(), sitemap(), tailwind(), image()],
+});
