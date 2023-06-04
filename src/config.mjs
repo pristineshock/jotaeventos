70e219f3bf5b6dac3580712554868c0ac2644bc2
matiasfacello <matiasfacello@gmail.com>
2023-06-04 01:13:09 -0300

    feat: small social rework

--- src/config.mjs
+++ src/config.mjs
@@ -10,6 +10,8 @@ export const SITE = {
   facebook: null,
   instagram: "https://www.instagram.com/jota_eventos/",
   whatsapp: "https://api.whatsapp.com/send?phone=+543435411954&text=Hola%20Jota%2C%20como%20estas%3F%20Me%20gustaria%20pedirte%20un%20presupuesto.",
+  email: "juliofacello22@gmail.com",
+  phone: "+5493435411954",
 
   googleCaptchaPublicKey: "6Lcb99IlAAAAAO8WpIz-6ebQiswQVxS9YZqTZ5W4",
   googleAnalyticsId: false,
